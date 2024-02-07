34f5321b0aca0cbbb1edd4137f6b21b749bc5c9a
Negiparas26 <parasnegi10101@gmail.com>
2024-02-08 00:45:26 +0530

    redux toolkit

---
+++ 09todo-reactToolkit/src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <>
+    <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+   </>
+  )
+}
+
+export default App
