d44ee570b498164a086e557c1b3f422a93cc607a
Negiparas26 <parasnegi10101@gmail.com>
2024-02-29 20:44:36 +0530

    mega

---
+++ Mega-Blog/src/components/container/Container.jsx
@@ -0,0 +1,8 @@
+
+// eslint-disable-next-line react/prop-types
+function Container({children}) {
+  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
+  
+}
+
+export default Container
